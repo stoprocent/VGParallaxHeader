4f4126cfd46565e5d9ad2692fde54a12b215cfb4
Marek Serafin <marek@snowheads.pl>
2014-11-30 14:03:48 +0100

    New example

---
+++ Example/Example/ThirdTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdTableViewController.h
+//  Example
+//
+//  Created by Marek Serafin on 30/11/14.
+//  Copyright (c) 2014 Marek Serafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdTableViewController : UIViewController
+
+@end
---
+++ Example/Example/ThirdTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  ThirdTableViewController.m
+//  Example
+//
+//  Created by Marek Serafin on 30/11/14.
+//  Copyright (c) 2014 Marek Serafin. All rights reserved.
+//
+
+#import "ThirdTableViewController.h"
+
+@implementation ThirdTableViewController
+
+@end
